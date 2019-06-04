0518f855d66966d87b7e224fdf02fff829cf1874
Cynopolis <quinn.henthorne@gmail.com>
2019-06-04 09:57:06 -0500

    Potentiometer Test Code FInished
    The code for measuring the angle of the front wheels is finished. Features include:
    Autoscaling: the program will figure out where the maximum and minimum value of the potentiometer is by itself.
    
    Value filtering: Often times the potentiomter can return a sudden spike in its value which htrows off the auto scaling. This no longer happens.

--- Arduino/pot_test/pot_test.ino
+++ Arduino/pot_test/pot_test.ino
@@ -8,16 +8,22 @@ float maxAngle = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  minAngle = analogRead(anglePin);
+  maxAngle = analogRead(anglePin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   measuredAngle = analogRead(anglePin);
   if(measuredAngle > maxAngle){
-    maxAngle = measuredAngle;
+    if((measuredAngle-maxAngle) < 5){
+      maxAngle = measuredAngle;
+    }
   }
   if(measuredAngle < minAngle){
-    minAngle = measuredAngle;
+    if((minAngle-measuredAngle) < 5){
+      minAngle = measuredAngle;
+    }
   }
   Serial.print("Current: ");
   Serial.print(measuredAngle);
@@ -26,6 +32,6 @@ void loop() {
   Serial.print(" Min: ");
   Serial.print(minAngle);
   Serial.print(" Actual Angle: ");
-  Serial.println((measuredAngle-minAngle-(maxAngle-minAngle)/2)/(maxAngle-minAngle));
+  Serial.println(((measuredAngle-minAngle-(maxAngle-minAngle)/2)/(maxAngle-minAngle))*60);
   delay(1);
 }
