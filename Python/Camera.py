8b057368a9369a3a398cc4031a7e2587dc7d49df
Cynopolis <quinn.henthorne@gmail.com>
2019-06-06 13:45:23 -0500

    Added a new python class for the kinect
    also added stuff thats auto generated to the.gitignore file

---
+++ Python/Camera.py
@@ -0,0 +1,17 @@
+import cv2
+
+class Camera:
+    def __init__(self, cameraIndex):
+        self.cap = cv2.VideoCapture(cameraIndex)
+        self.
+        return
+
+    def getImage(self):
+        return
+
+    def processImage(self):
+        return
+
+class depthCamera(Camera):
+    def __init__(self):
+        return
